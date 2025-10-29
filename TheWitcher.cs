3ae21e0c5ebabc0492eccf6f6b107260e56e3d2f
Altair Wei <altair_wei@outlook.com>
2025-10-29 23:10:13 +0800

    bump version

--- TheWitcher.cs
+++ TheWitcher.cs
@@ -13,7 +13,7 @@ public partial class TheWitcher : Mod
     public override string Author => "";
     public override string Name => "The Witcher";
     public override string Description => "Coder: Altair\nDesigner: Altair, 北境的救世主, 蓝火\nArtist: 海兰芝, 北境的救世主, 六月下午的猫";
-    public override string Version => "0.3.7";
+    public override string Version => "0.4.0";
     public override string TargetVersion => "0.9.2.13";
 
     public override void PatchMod()
